de75d8651edea5641fdd8375089f4cb8c062e9a7
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-07-04 14:20:58 +0400

    implemented file upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,18 @@ import {
   Body,
   Controller,
   Get,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GenerateCollectionDto } from './dto/generateCollection.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { diskStorage } from 'multer';
+import { mkdir } from 'fs';
 
 @Controller()
 export class AppController {
@@ -16,7 +23,30 @@ export class AppController {
   @UsePipes(new ValidationPipe())
   generateCollection(
     @Body() generateCollectionDto: GenerateCollectionDto,
-  ): string {
+  ): Promise<any> {
     return this.appService.generateCollection(generateCollectionDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FilesInterceptor('files', Infinity, {
+      limits: {
+        fileSize: 2 * 1024 * 1024,
+      },
+      storage: diskStorage({
+        destination: (req, file, callback) => {
+          const dirName = './layers_' + req.headers.timestamp;
+          mkdir(dirName, () => {
+            callback(null, dirName);
+          });
+        },
+        filename: (req, file, callback) => {
+          callback(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    return 'uploaded successfully';
   }
 }
